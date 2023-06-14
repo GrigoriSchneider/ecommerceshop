113b1e4dac7f1a6a872c2c1555ed0f146bac4f33
unknown <grigorischneider@gmail.com>
2023-06-14 18:30:51 +0200

    removed semicolon

--- src/routes/home/home.component.js
+++ src/routes/home/home.component.js
@@ -5,7 +5,7 @@ import Directory from "../../components/directory/directory.component";
 const Home = () => {
   return (
     <>
-      <Directory />;
+      <Directory />
       <Outlet />
     </>
   );
